6671c983eb28b6ee3cdf0b99c5895ca6f90a5bfe
praneethdoure <praneethdoure@gmail.com>
2024-07-14 15:27:12 +0530

    added new tmp files Signed-off-by: praneethdoure <praneethdoure@gmail.com>

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 
+from doure_test.tmp.tmp2 import test
+
 print("hello pdoure")
+
+print("hi this is another handson commit")
+
+test("first commit","second commit")
---
+++ tmp/tmp2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print("hello pdoure")
+
+print("hi this is another handson commit")
+
+def test(a,b):
+    if a == "first commit":
+        print("this is first commit")
+    if b == "second commit":
+        print(" this is second commit ")
+    else :
+        print("None matched")
+
+
